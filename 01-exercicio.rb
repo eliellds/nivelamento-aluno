96107d0868997687d7a98ab0920e50d74a887b72
Eliel Lopes <eliel.lds@gmail.com>
2021-07-08 15:23:34 -0300

    primeiro-exercicio

---
+++ 01-exercicio.rb
@@ -0,0 +1,10 @@
+puts "Quantidade de horas em um ano (não bissexto)"
+puts 24 * 365
+puts "Quantidade de minutos em uma década (somando os anos bissextos)"
+puts 365 * 10 * 24 * 60 + (24 * 60 * 3)
+puts "Minha idade em segundos (somando os anos bissextos)"
+puts 365 * 26 * 24 * 60 * 60 + (24 * 60 * 60 * 7)
+puts "Quantidade de chocolates que pretendo comer na vida"
+puts 56 * 90
+puts "Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
+puts 1232000000 / 365 / 24 / 60 / 60
