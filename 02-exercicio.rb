cc84f3d8a576f743465ac1474bf4e65534146895
Eliel Lopes <eliel.lds@gmail.com>
2021-07-08 17:24:32 -0300

    02-exercicio-variaveis

---
+++ 02-exercicio.rb
@@ -0,0 +1,16 @@
+horasDia = 24
+minutosDia = horasDia * 60
+segundosDia = minutosDia * 60
+diasAno = 365
+idadeSegundos = 1232000000
+
+puts "Quantidade de horas em um ano (não bissexto)"
+puts horasDia * diasAno
+puts "Quantidade de minutos em uma década"
+puts minutosDia * diasAno * 10
+puts "Minha idade em segundos"
+puts segundosDia * diasAno * 26
+puts "Quantidade de chocolates que pretendo comer na vida"
+puts 56 * 90
+puts "Se minha idade é 1232 milhões de segundos, qual é minha idade em anos?"
+puts idadeSegundos / (segundosDia * diasAno)
