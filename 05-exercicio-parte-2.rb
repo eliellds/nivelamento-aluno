9e9a89f06706ca1a04acd035bd5a9af8692a79d0
Eliel Lopes <eliel.lds@gmail.com>
2021-07-13 15:03:34 -0300

    05-exercicio-parte-2-v2

--- 05-exercicio-parte-2.rb
+++ 05-exercicio-parte-2.rb
@@ -1,24 +1,15 @@
 puts "Programa para calcular estimativa de batimentos cardíacos baseado na idade do usuário."
 puts
-puts "Olá!"
-print "Por favor, informe seu nome: "
+print "Olá!\nPor favor, informe seu nome: "
 nome = gets.chomp
 print "Agora, informe sua idade: "
 anos = gets.chomp
 anos = anos.to_i
 puts
 
-#funcao para converter anos para dias
-def converterAnosParaDias(anos)
-    anosParaDias = anos * 365
-    return anosParaDias
-end
-
-anosParaDias = converterAnosParaDias(anos)
-
 #funcao para calcular estimativa de batimentos baseado na quantidade de anos informada
 def calculaEstimativaBatimentos(anos)
-    estimativaBatimentos = converterAnosParaDias(anos) * 24 * 60 * 80
+    estimativaBatimentos = anos * 365 * 24 * 60 * 80
     return estimativaBatimentos
 end
 
