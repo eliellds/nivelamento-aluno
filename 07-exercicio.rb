69626dae0ed82259008efdbb27affc71a36c02e6
Eliel Lopes <eliel.lds@gmail.com>
2021-07-15 01:18:38 -0300

    07-exercicio-jogo-v2

--- 07-exercicio.rb
+++ 07-exercicio.rb
@@ -15,21 +15,19 @@ chute = gets.chomp
 def validadorChute(chute)
     if chute == "0"
         return true
-    elsif chute.to_i > 0 && chute.to_i <= 100 && chute.to_i
+    elsif chute.to_i > 0 && chute.to_i <= 100
         return true
     else
         return false
     end
 end
 
-if validadorChute(chute)
-    chute = chute.to_i
-else 
-    puts "Insira apenas algorismos numéricos inteiros entre 0 e 100. (ex.: 0, 4,... 98)"
-end
-
-if numero == chute
+if validadorChute(chute) == false 
+puts "Chute inválido! Insira apenas números inteiros entre 0 e 100."
+elsif chute = chute.to_i
+    if numero == chute
     puts "Acertou!"
-else
+    else
     puts "Errou!"
+    end
 end
